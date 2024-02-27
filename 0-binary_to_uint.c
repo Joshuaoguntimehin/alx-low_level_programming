34662a6f426a2220c0b8ce8d59a1eac542f2c64b
joshua oguntimehin <joshuaoguntimehin46@gmail.com>
2024-02-27 04:19:05 -0800

    new

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -1,8 +1,30 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#include "main.h"
 
-#define SIZE 1024
+/**
+ * binary_to_uint - function converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int num;
 
-#endif
-
+	num = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
+}
 
