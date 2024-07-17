458cdfb2670d86e3b7d0dc085c6b7651e94aa823
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-17 12:45:11 +0100

    alx tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,9 +7,10 @@
  * value - value is the value to search for
  */
 
-int linear_search(int *array, size_t size, int value){
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i = 0;
-	if (array == NULL)
+	if (array == NULL) 
 		return -1;
 	for (i = 0; i < size; i++) {
 		printf("Value checked array[%lu] = %d\n", i, array[i]);
