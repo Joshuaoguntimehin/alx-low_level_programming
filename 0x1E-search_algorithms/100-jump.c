7f62f22d00d8beb948250c746425973c32673f1c
joshuaoguntimehin <joshuaoguntimehin46@gmail.com>
2024-07-17 12:56:01 +0100

    thanks michael

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "search_algo.h"
+/*
+ * main: 
+ * array: array is a pointer to the first element of the array to search in
+ * value: value is the value to search for
+ * size: size is the number of elements in array
+ * return: return i if successfull else return -1
+ */
+int jump_search(int *array, size_t size, int value){
+	size_t i;
+	size_t 
